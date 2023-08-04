99e69dd4093369cdda1eb3ae81466a3084b201e5
jojo <luizj1718@gmail.com>
2023-08-04 12:29:33 -0300

    initial commit

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const cors = require("cors");
+const axios = require("axios");
+require("dotenv").config();
+
+const app = express();
+app.use(express.json());
+app.use(cors({ origin: true }));
+const port = process.env.PORT || 3000;
+
+app.post("/signup", async (req, res) => {
+  const { username, secret } = req.body;
+  try {
+    const r = await axios.post(
+      "https://api.chatengine.io/users/",
+      { username, secret },
+      { headers: { "Private-Key": process.env.CHAT_ENGINE_PRIVATE_KEY } }
+    );
+    return res.status(r.status).json(r.data);
+  } catch (e) {
+    return res.status(e.response.status).json(e.response.data);
+  }
+});
+
+app.post("/login", async (req, res) => {
+  const { username, secret } = req.body;
+  try {
+    const r = await axios.get("https://api.chatengine.io/users/me/", {
+      headers: {
+        "Project-ID": process.env.CHAT_ENGINE_PROJECT_ID,
+        "User-Name": username,
+        "User-Secret": secret,
+      },
+    });
+    return res.status(r.status).json(r.data);
+  } catch(e) {
+    return res.status(e.response.status).json(e.response.data);
+  }
+});
+
+app.listen(port, () => {
+    console.log(`Servidor rodando em http://localhost:${port}`);
+  });
