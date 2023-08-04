8e5d06e79b47e5af15096ac3eda4f73f0fcf18ec
jojo <luizj1718@gmail.com>
2023-08-04 18:11:40 -0300

    feacht: cors

--- index.js
+++ index.js
@@ -3,10 +3,14 @@ const cors = require("cors");
 const axios = require("axios");
 require("dotenv").config();
 
+const corsOptions = {
+  origin: "https://chat-s9df.onrender.com",
+  optionsSuccessStatus: 200,
+};
 const app = express();
 app.use(express.json());
-app.use(cors({ origin: true }));
-const port = process.env.PORT || 3000;
+app.use(cors(corsOptions));
+const port = process.env.PORT || 3001;
 
 app.post("/signup", async (req, res) => {
   const { username, secret } = req.body;
@@ -33,11 +37,11 @@ app.post("/login", async (req, res) => {
       },
     });
     return res.status(r.status).json(r.data);
-  } catch(e) {
+  } catch (e) {
     return res.status(e.response.status).json(e.response.data);
   }
 });
 
 app.listen(port, () => {
-    console.log(`Servidor rodando em http://localhost:${port}`);
-  });
+  console.log(`Servidor rodando em http://localhost:${port}`);
+});
